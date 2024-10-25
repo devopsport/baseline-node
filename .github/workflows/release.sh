b868b51962a09716ff6edd52465b81ab026b6e88
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-10-25 14:16:47 -0500

    debug

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ "${GITHUB_REF_NAME}" = "master" ]
+then
+  ENV="prod"
+else
+  ENV="staging"
+fi
+
 echo "PROJECT_NAME: ${PROJECT_NAME}"
 echo "SERVICE_NAME: ${SERVICE_NAME}"
 echo "GITHUB_REF_NAME: ${GITHUB_REF_NAME}"
+echo "ENV: ${ENV}"
